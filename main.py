462f870b73e012fba320762b81a3741f25bf2b39
Jonathan Leimbach <jonathan.leimbach2408@gmail.com>
2024-03-19 00:46:04 +0100

    Added post_quote() function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from pprint import pprint
 import requests
+import tweepy
 import os
 
 load_dotenv()
@@ -16,11 +17,30 @@ def get_quote():
 
 
 def post_quote(author="foo", quote="bar"):
-    pass
+    api_key = os.getenv('API_KEY')
+    api_key_secret = os.getenv('API_KEY_SECRET')
+    access_token = os.getenv('ACCESS_TOKEN')
+    access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
 
+    auth = tweepy.OAuthHandler(
+        api_key,
+        api_key_secret
+    )
+
+    auth.set_access_token(
+        access_token,
+        access_token_secret
+    )
+
+    api = tweepy.API(auth)
+
+    tweet_text = f"{quote}\n{author}"
+    api.update_status(tweet_text)
 
 if __name__ == "__main__":
     quote_raw = get_quote()
     author = quote_raw[0]['author']
     quote = quote_raw[0]['quote']
     print(f"Author: {author}\nQuote: {quote}")
+
+    post_quote(author, quote)
